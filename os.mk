77f2f5d4cbcac7fb70125538f3df2a3c29085a73
Martin Read <mpread@chiark.greenend.org.uk>
2009-11-26 09:06:57 +0000

    added makefile includee for OS-specific things

---
+++ os.mk
@@ -0,0 +1,5 @@
+# If you are on an operating system which expects native-code executables to
+# have a distinctive suffix (e.g. Mickeysoft's .EXE extension), define it here.
+# Those of us on operating systems with a sensible heritage don't need to care
+# about such things :)
+EXECUTABLE_SUFFIX=
