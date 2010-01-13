7c56edd609743322f47d929b7363b5052f97515a
Martin Read <mpread@chiark.greenend.org.uk>
2010-01-13 00:33:11 +0000

    Boilerplate and new operations in mathops.hh

--- src/mathops.hh
+++ src/mathops.hh
@@ -1,6 +1,28 @@
-// mathops.hh
-//
-// Copyright 2009 Martin Read
+/* mathops.hh
+ *
+ * Copyright 2009 Martin Read
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef LIBMRL_MATHOPS_HH
 #define LIBMRL_MATHOPS_HH
@@ -22,6 +44,17 @@ namespace libmrl
     template <typename T> inline T sign(const T& orig)
     {
         return (orig != 0) ? ((orig < 0) ? T(-1) : T(1)) : T(0);
+    }
+    /* div_up - integer divide with upward rounding and no FP conversion.
+     *
+     * If you want to know why I don't just invoke FP arithmetic then call
+     * ceil(): It's _messy_ on RISC architectures, since they tend not to have
+     * a convert-integer-and-place-in-FPR instruction. (x86 *does* have such
+     * an instruction).
+     */
+    template <typename T> inline T div_up(T orig, T divisor)
+    {
+        return (orig + (divisor - 1)) / divisor;
     }
 }
 
